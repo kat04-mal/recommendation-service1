8295d6b71270a29c6b142201524142ab01cdba6b
kat04-mal <k-04maleeva@yandex.ru>
2026-07-13 23:22:30 +0300

    Initial commit

---
+++ recommendation-service1/src/test/java/ru/starbank/recommendation_service1/RecommendationService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.starbank.recommendation_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
